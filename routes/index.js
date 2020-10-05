648f2bf9bbc694b5c20bb4114c4f98558de0d80f
kavithad04 <kavithanjali123@gmail.com>
2020-10-04 17:21:00 -0700

    Fix app.get

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,14 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
-var pg = require('pg');
-var pool = new pg.Pool()
+const {Pool} = require("pg");
+var conStr = process.env.DATBASE_UR;
+const pool = new Pool({
+  connectionString: conStr
+});
+
 router.get('/db', function (request, response) {
-  pool.connect(process.env.DATABASE_URL, function(err, client, done) {
+  pool.connect(function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
