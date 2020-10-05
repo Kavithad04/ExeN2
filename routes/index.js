fc119ccd265bf2f2455670c2370d4b37ca7f985c
kavithad04 <kavithanjali123@gmail.com>
2020-10-04 17:55:33 -0700

    Fix app.get

--- routes/index.js
+++ routes/index.js
@@ -23,12 +23,13 @@ router.get('/db', function (request, response) {
       done();
       if (err)
       {
-        console.log(client);
+        console.info(" Printing the client error:" + client);
         client.release();
-        console.error(err); response.send("Error " + err); }
-      else
-      { response.render('pages/db', {results: result.rows} );
-      client.release();
+        console.error(err); response.send("Error " + err);
+      } else {
+        console.info("Connection is success:")
+        response.render('pages/db', {results: result.rows} );
+        client.release();
       }
     });
   });
